5951d7c5bf995ee305b7ab2f5032a6947cffef9c
ckdwlsrh <ckdwls6504@gmail.com>
2025-08-23 11:03:36 +0900

    initial Commit

---
+++ hyo-banking-be/src/main/java/org/atmgigi/hyobankingbe/HyoBankingBeApplication.java
@@ -0,0 +1,13 @@
+package org.atmgigi.hyobankingbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyoBankingBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyoBankingBeApplication.class, args);
+	}
+
+}
---
+++ hyo-banking-be/src/test/java/org/atmgigi/hyobankingbe/HyoBankingBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.atmgigi.hyobankingbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyoBankingBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
