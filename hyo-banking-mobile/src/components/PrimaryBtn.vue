4dd8ba80a195c0003a8a88e6a9f2460e61937627
donggeon Na <ndmb2012@gmail.com>
2025-09-06 16:07:39 +0900

    ✨ [feat][mobile] 재사용 가능한 컴포넌트와 상수 추가 - 일관된 스타일링이 포함된 PrimaryBtn 컴포넌트 생성 - 설정을 위한 애플리케이션 상수 추가 - 적절한 컴포넌트 구성 구현

---
+++ hyo-banking-mobile/src/components/PrimaryBtn.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    :disabled="disabled"
+    @click="handleClick"
+    class="bg-kb-brown-100 rounded-2xl py-6 text-white font-bold text-center hover:scale-105 active:scale-95 active:brightness-75 transition duration-300 shadow-sm cursor-pointer"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script setup>
+  const props = defineProps({
+    text: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  });
+
+  const emit = defineEmits(['click']);
+
+  const handleClick = () => {
+    if (!props.disabled) {
+      emit('click');
+    }
+  };
+</script>
---
+++ hyo-banking-mobile/src/constants/index.js
@@ -0,0 +1,10 @@
+export const TASK_TYPES = {
+  DEPOSIT: 'deposit', // 입금
+  WITHDRAW: 'withdraw', // 출금
+  TRANSFER: 'transfer', // 송금,
+};
+
+export const PAYMENT_TYPES = {
+  CARD: 'card',
+  BANKBOOK: 'bankbook',
+};
