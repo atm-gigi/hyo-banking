6dd10517d2688c539599e72faf5cc4021c80d246
donggeon Na <ndmb2012@gmail.com>
2025-09-07 15:08:34 +0900

    ✨ [feat][mobile] 버튼 컴포넌트 추가 - PrimaryBtn: 로딩/비활성화 상태 지원하는 기본 버튼 - IconBtn: 아이콘과 텍스트가 있는 버튼 컴포넌트 - GoBackBtn: 뒤로가기 기능을 제공하는 재사용 가능한 버튼

---
+++ hyo-banking-mobile/src/components/buttons/IconBtn.vue
@@ -0,0 +1,34 @@
+<template>
+  <button @click="handleClick" class="cursor-pointer active:brightness-50 transition w-full">
+    <div class="flex flex-row justify-between items-center">
+      <img :src="iconSrc" :alt="iconAlt" class="size-9" />
+      <span class="font-bold pl-5 text-start w-full">{{ text }}</span>
+      <img class="size-4" width="30" height="30" :src="ICON_URLS.FORWARD" alt="forward" />
+    </div>
+  </button>
+</template>
+
+<script setup>
+  import { ICON_URLS } from '@/constants';
+
+  defineProps({
+    text: {
+      type: String,
+      required: true,
+    },
+    iconSrc: {
+      type: String,
+      required: true,
+    },
+    iconAlt: {
+      type: String,
+      required: true,
+    },
+  });
+
+  const emit = defineEmits(['click']);
+
+  const handleClick = () => {
+    emit('click');
+  };
+</script>
