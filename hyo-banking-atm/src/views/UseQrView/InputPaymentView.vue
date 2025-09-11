7bfbf77ce588c5fcc324c2d9251415e6928e25d0
ckdwlsrh <ckdwls6504@gmail.com>
2025-09-11 11:05:48 +0900

    ✨[feat][fe] 매크로때 비밀번호 인증 추가 - issue: #17

--- hyo-banking-atm/src/views/UseQrView/InputPaymentView.vue
+++ hyo-banking-atm/src/views/UseQrView/InputPaymentView.vue
@@ -68,7 +68,7 @@
 
   const handleEnter = () => {
     router.push({
-      name: 'handle-macro-step',
+      name: 'input-password-withdraw',
       params: { steps: JSON.stringify(steps) },
       query: { code },
     });
@@ -100,7 +100,11 @@
     newVal => {
       console.log(newVal);
       if (newVal.length === 0) {
-        router.push({ name: 'handle-macro-step', params: { steps: JSON.stringify(steps) } });
+        router.push({
+          name: 'input-password-withdraw',
+          params: { steps: JSON.stringify(steps) },
+          query: { code },
+        });
       }
     },
     { immediate: true }
